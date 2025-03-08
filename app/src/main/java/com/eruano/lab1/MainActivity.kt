917d749c533616cb5e9400661b752f406c91b3f5
Eruano <fdgolovlev@gmail.com>
2025-03-08 16:10:23 +0300

    feat: add updating func to update friends count

--- app/src/main/java/com/eruano/lab1/MainActivity.kt
+++ app/src/main/java/com/eruano/lab1/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.eruano.lab1
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.eruano.lab1.models.Post
 
+
 class MainActivity : ComponentActivity() {
     private lateinit var postsRecyclerView: RecyclerView
     private lateinit var postAdapter: PostAdapter
+    private lateinit var friendsCountTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +33,12 @@ class MainActivity : ComponentActivity() {
             .error(R.drawable.error_placeholder)
             .into(profileImage)
 
-        postsRecyclerView = findViewById(R.id.postsRecyclerView)
 
+        friendsCountTextView = findViewById(R.id.friendsCount)
+
+        updateFriendsCount(10, 2)
+
+        postsRecyclerView = findViewById(R.id.postsRecyclerView)
 
         postsRecyclerView.setLayoutManager(LinearLayoutManager(this))
 
@@ -41,25 +48,49 @@ class MainActivity : ComponentActivity() {
                 "Заголовок 1",
                 "Текст поста 1",
                 "31 дек 2017",
-                "https://dummyimage.com/600x300/000/fff&text=Hello+World"
+                "https://dummyimage.com/600x300/000/fff&text=Hello+World",
+                0
             ),
             Post(
                 "Заголовок 2",
                 "Текст поста 2",
                 "1 янв 2018",
-                "https://dummyimage.com/600x300/ff0000/ffffff"
+                "https://dummyimage.com/600x300/ff0000/ffffff",
+                0
             ),
             Post(
                 "Заголовок 3",
                 "Текст поста 3",
                 "2 янв 2018",
-                "https://dummyimage.com/600x300/000/fff"
+                "https://dummyimage.com/600x300/000/fff",
+                0
             )
         )
-
 
         postAdapter = PostAdapter(postList)
 
         postsRecyclerView.setAdapter(postAdapter)
+    }
+
+    private fun updateFriendsCount(friendsCount: Int, requestsCount: Int) {
+        val friendsText = "Друзей: $friendsCount"
+        val requestsText = "Заявок: $requestsCount"
+        val fullText = "$friendsText · $requestsText"
+
+        val spannableString = SpannableString(fullText)
+        spannableString.setSpan(
+            ForegroundColorSpan(Color.BLACK),
+            0,
+            friendsText.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableString.setSpan(
+            ForegroundColorSpan(Color.BLUE),
+            friendsText.length,
+            fullText.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        friendsCountTextView.text = spannableString
     }
 }
--- app/src/main/java/com/eruano/lab1/PostAdapter.kt
+++ app/src/main/java/com/eruano/lab1/PostAdapter.kt
@@ -9,7 +9,6 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.eruano.lab1.models.Post
-import java.util.Locale
 
 class PostAdapter(private val postList: List<Post>) : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
 
